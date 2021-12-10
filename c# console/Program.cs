77d91665104aec8fc17416e25ad9a22cd0d0a32e
Devrim Mehmet Pattabanoğlu <devrimmehmet@gmail.com>
2021-12-10 20:33:28 +0300

    CSharp-Console-Patika

---
+++ c# console/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace c__console
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hoş Geldiniz!");
+            Console.WriteLine("Adınızı Giriniz :");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyadınızı Giriniz :");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Merhaba "+name+" "+surname);
+           
+           
+        }
+    }
+}
