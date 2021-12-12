e525ebd872b5a564f084d78d251e22b50cbec92b
Devrim Mehmet Pattabanoğlu <devrimmehmet@gmail.com>
2021-12-12 15:20:15 +0300

    TipDonusumleri

---
+++ Tip-Donusumleri/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tip_Donusumleri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
