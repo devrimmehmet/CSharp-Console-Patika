2c3cab1177101b17e1fba77c6c90913c52b2538b
Devrim Mehmet Pattabanoğlu <devrimmehmet@gmail.com>
2021-12-14 17:17:19 +0300

    diziler

---
+++ Diziler/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Diziler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] renkler =new string[5];
+            string [] hayvanlar={"Kedi","Köpek","Kuş","Maymun"}; //boyutu 4
+            int [] dizi;
+            dizi= new int [5];
+
+            // Dizilere değer atama ve Erişim
+            renkler[0]="Mavi";
+            Console.WriteLine(hayvanlar[1]);
+            dizi[3]=10;
+            Console.WriteLine(dizi[3]);
+            Console.WriteLine(renkler[0]);
+
+            //Döngülerle dizi kullanımı
+            //Klavyeden girilen n tane sayının ortalamasını alan program
+            
+            Console.WriteLine("Lütfen Dizinin Eleman Sayısını Giriniz.");
+            int diziUzunlugu=int.Parse(Console.ReadLine());
+            int[] sayıDizisi = new int[diziUzunlugu];
+            for(int i=0; i<diziUzunlugu;i++)
+            {
+                Console.WriteLine("Lütfen {0}. sayıyı giriniz",i+1);
+                sayıDizisi[i]=int.Parse(Console.ReadLine());
+            }
+            int toplam=0;
+            foreach (var sayi in sayıDizisi)
+            {
+                toplam+=sayi;
+            }
+            Console.WriteLine("Ortalama : "+ toplam/diziUzunlugu);
+        }
+    }
+}
