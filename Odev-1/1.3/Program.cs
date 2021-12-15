45b70cb6569677edeff0a97d3eba80a0cc86c9f9
Devrim Mehmet Pattabanoğlu <devrimmehmet@gmail.com>
2021-12-15 23:55:02 +0300

    odev1.3

---
+++ Odev-1/1.3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _1._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin (n). 
+            //Sonrasında kullanıcıdan n adet kelime girmesi isteyin. 
+            //Kullanıcının girişini yaptığı kelimeleri sondan başa doğru console'a yazdırın.
+             DMP:
+            Console.WriteLine("Lütfen Pozitif Bir Tam Sayı Giriniz!");
+            
+          try
+          {
+               int n;
+               
+            n=Convert.ToInt32(Console.ReadLine());
+          Console.WriteLine("Şimdi "+n+" adet kelime giriniz :");
+
+            string[] dizi=new string[n];
+            for(int i=0;i<n;i++){
+                Console.WriteLine((i+1)+". Kelime :");
+                dizi[i]=Console.ReadLine();
+            }
+            Console.WriteLine("***Girdiğin Kelimeler Sondan Başa Doğru Sıralanırsa;");
+            Array.Reverse(dizi);
+            for(int i=0;i<n;i++){
+                
+               Console.WriteLine(dizi[i]);
+            }
+            Console.WriteLine("***Yukarıdaki sırada olur.");
+          }
+          catch (Exception)
+          {
+              Console.WriteLine("Pozitif Sayı girsene gardaşım. Patlattın Programı");
+              goto DMP;
+          }
+        }
+    }
+}
