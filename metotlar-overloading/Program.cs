c1365b47f97b1bd4054450248e5d1ed1d9997309
Devrim Mehmet Pattabanoğlu <devrimmehmet@gmail.com>
2021-12-15 22:27:04 +0300

    metot overloading

---
+++ metotlar-overloading/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace metotlar_overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //out parametreler
+            string sayi="999";
+            int outSayi;
+           bool sonuc= int.TryParse(sayi,out outSayi);
+            
+                if(sonuc){
+                    Console.WriteLine("Başarılı!");
+                    Console.WriteLine(outSayi);
+                    
+                }else{
+                    Console.WriteLine("Başarısız!");
+                }
+            
+            Metotlar instance=new Metotlar();
+            instance.Topla(4,5,out int toplamSonucu);
+            Console.WriteLine(toplamSonucu);
+
+
+            // Metot Aşırı Yükleme - Overloading
+            int ifade = 999;
+            instance.EkranaYazdir(Convert.ToString(ifade));
+            instance.EkranaYazdir(ifade);
+            instance.EkranaYazdir("Devrim","Mehmet");
+            //Metot imzasına göre karar veriyor
+            //MetotAdı + Parametre sayısı + parametre
+        }
+    }
+    class Metotlar 
+    {
+        public void Topla(int a,int b, out int toplam)
+        {
+            toplam=a+b;
+        }
+        public void EkranaYazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+        public void EkranaYazdir(int veri)
+        {
+            Console.WriteLine(veri);
+        }
+        public void EkranaYazdir(string veri1,string veri2)
+        {
+            Console.WriteLine(veri1+veri2);
+        }
+    }
+}
