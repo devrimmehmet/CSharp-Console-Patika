f7fdc0bd589475022ccc9d627a999ad3d60300c3
Devrim Mehmet Pattabanoğlu <devrimmehmet@gmail.com>
2021-12-19 01:25:32 +0300

    odev-2-koleksiyonlar-soru-3

---
+++ Odev-2/Koleksiyonlar-Soru-3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar_Soru_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                //Klavyeden girilen cümle içerisindeki sesli harfleri bir dizi içerisinde saklayan ve dizinin elemanlarını sıralayan programı yazınız.
+
+                Console.WriteLine("Bir Cümle Giriniz.");
+                string cumle=Console.ReadLine();
+                 string liste = "AEİIOÖUÜ" + "aeiıoöuü";
+                List<string> SesliHarfler=new List<string>();
+            
+            int sayac = 0;
+            for (int i = 0; i < cumle.Length; i++)
+            {
+                //harf harf listemizde varmı varsa sayacımız 1 harf var diyor. kontrol ediyoruz.
+                if (liste.Contains(cumle[i]))
+                {
+                    sayac++;
+                    SesliHarfler.Add(cumle[i].ToString());
+                }
+            }
+            Console.WriteLine("toplam {0} tane sesli harf vardır.", sayac);
+            SesliHarfler.Sort();
+            Console.Write("Sıralı şekilde listesi: ", sayac);
+            foreach(var item in SesliHarfler)
+            {
+                Console.Write(item+" ");
+            }
+        }
+    }
+}
